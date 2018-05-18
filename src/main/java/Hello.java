56c7d259ef83ffab6e480759733cb9015f9bb0a4
bertukan <bertukan2017@gmail.com>
2018-05-18 12:38:04 -0500

    new adding maven

---
+++ src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public void print(){
+        System.out.println("Hello Git");
+
+        System.out.println("hello2");
+
+        System.out.println("i hate you saladin");
+    }
+}
